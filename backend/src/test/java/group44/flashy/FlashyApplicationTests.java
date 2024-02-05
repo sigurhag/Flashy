9184848c1a0a151b8fa3f9c0a17cf3b1a50ddf32
Sigurd Hagen <sigurd.hagen@ntnu.no>
2024-02-05 13:50:45 +0100

    Resolve "Setting up a development environment"

---
+++ backend/src/test/java/group44/flashy/FlashyApplicationTests.java
@@ -0,0 +1,13 @@
+package group44.flashy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
