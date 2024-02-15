8b80b48983e2440f0df6041316bd3c53d1e8e691
Steinar Stuestøl <steistu@stud.ntnu.no>
2024-02-15 11:32:18 +0100

    Create user extended admin class shell backend

---
+++ backend/src/test/java/group/flashy/AdminTest.java
@@ -0,0 +1,10 @@
+package group.flashy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class AdminTest {
+    
+}
