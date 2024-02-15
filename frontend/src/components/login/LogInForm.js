088e48490d30919328374dab2dde16b24aa8174e
sigurhag <sigurhag@ntnu.no>
2024-02-15 15:03:13 +0100

    feat: added axios functions to login and register

--- frontend/src/components/login/LogInForm.js
+++ frontend/src/components/login/LogInForm.js
@@ -1,30 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import LogInButton from "./Button";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const LogInForm = () => {
-    return (
-        <div className='vh-100 flex flex-column justify-center items-center'>
-            <h1 className="f1 mb3">FLASHY</h1>
-            <label className="mb3">
-                <input 
-                    style={{borderRadius: '90px'}}
-                    type="text" 
-                    className="grow pa2 font-color" 
-                    placeholder="Username" 
-                />
-            </label>
-            <label className="mb3">
-                <input 
-                        style={{borderRadius: '90px'}}
-                        type="password" 
-                        className="grow pa2 font-color" 
-                        placeholder="Password" 
-                    />
-            </label>
-            <Link to="/home"><LogInButton /></Link>
-        </div>
-    );
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default LogInForm
+  const handleLoginPress = async () => {
+    try {
+      const response = await axios.post("", { //Link to spring 
+        username: username,
+        password: password,
+      });
+
+      if (response.data.success) {
+        console.log("approved login");
+        // Might want to use token or cookies here to remember logged in user
+      } else {
+        console.log("error login in");
+      }
+    } catch (error) {
+      console.error("An unexpected error occurred:", error);
+    }
+  };
+
+  return (
+    <div className='vh-100 flex flex-column justify-center items-center'>
+      <h1 className="f1 mb3">FLASHY</h1>
+      <label className="mb3">
+        <input 
+          style={{borderRadius: '90px'}}
+          type="text" 
+          className="grow pa2 font-color" 
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label className="mb3">
+        <input 
+          style={{borderRadius: '90px'}}
+          type="password" 
+          className="grow pa2 font-color" 
+          placeholder="Password" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+        <Link to="/home">
+          <LogInButton onClick={handleLoginPress}/>
+        </Link>
+    </div>
+  );
+};
+
+export default LogInForm;
--- frontend/src/components/registration/RegistrationForm.js
+++ frontend/src/components/registration/RegistrationForm.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import RegisterButton from "./Button";
 import { Link } from "react-router-dom";
 import BackButton from "./BackButton";
+import axios from "axios";
 
 const RegisterForm = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+
+    const handleRegisterPress = async () => {
+        try {
+        const response = await axios.post("", {
+            username: username,
+            email: email,
+            password: password,
+            password2: password2
+        });
+        if (response.data.sucess) {
+            console.log("Registration successful!");
+            //Save information about logged in user
+        } else {
+            console.log("error with registration")
+        } 
+    } catch(error) {
+            console.error("unexpected error: ", error);
+        }
+    }
+
     return (
         <div className='vh-100 flex flex-column justify-center items-center'>
             <h1 className="f1 mb3">FLASHY</h1>
@@ -13,6 +38,8 @@ const RegisterForm = () => {
                     type="text" 
                     className="grow pa2 font-color" 
                     placeholder="Username" 
+                    value={username}
+                    onChange= {(e) => setUsername(e.target.value)}
                 />
             </label>
             <label className="mb3">
@@ -21,6 +48,8 @@ const RegisterForm = () => {
                     type="text" 
                     className="grow pa2 font-color" 
                     placeholder="E-mail" 
+                    value={email}
+                    onChange= {(e) => setEmail(e.target.value)}
                 />
             </label>
             <label className="mb3">
@@ -29,6 +58,8 @@ const RegisterForm = () => {
                         type="password" 
                         className="grow pa2 font-color" 
                         placeholder="Password" 
+                        value={password}
+                        onChange= {(e) => setPassword(e.target.value)}
                     />
             </label>
             <label className="mb3">
@@ -37,10 +68,12 @@ const RegisterForm = () => {
                         type="password" 
                         className="grow pa2 font-color" 
                         placeholder="Repeat Password" 
+                        value={password2}
+                        onChange= {(e) => setPassword2(e.target.value)}
                     />
             </label>
-            <Link to="/home"><RegisterButton /></Link>
-            <Link to="/"><BackButton /></Link>
+            <Link to="/home"><RegisterButton onClick={handleRegisterPress} /></Link>
+            <Link to="/"><BackButton/></Link>
         </div>
     );
 }
